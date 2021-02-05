835fb512d1b88fbd557278e32a4777e53c617087
S K B <47430686+shravan20@users.noreply.github.com>
2021-02-05 08:14:10 +0530

    Add files via upload

---
+++ autobinservo/autobinservo.ino
@@ -0,0 +1,53 @@
+
+#include<Servo.h>
+
+Servo x;
+int echo=2;
+int trig=3;
+int dist=0;
+
+int dist_cal()
+{
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(20);
+  digitalWrite(trig,LOW); 
+
+  float duration=pulseIn(echo,HIGH);
+  float xdist=duration/29/2;
+  return (int) xdist;
+}
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+}
+
+void loop() 
+{
+  dist=dist_cal();
+  Serial.println("\nDISTANCE:"+dist);
+  
+  if(dist<20)
+  {   
+    x.attach(4);
+    for(int i=0;i<=180;i+=20)
+    {
+     
+      x.write(i);
+      delay(250);
+     
+    }
+  }
+  else 
+  {
+   
+    x.attach(4);
+    delay(1000);
+    x.detach();
+  }
+  
+}
