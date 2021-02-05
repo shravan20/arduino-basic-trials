1f9195135d5a132c0faa927d49e17029b4ff33c9
S K B <47430686+shravan20@users.noreply.github.com>
2021-02-05 08:18:56 +0530

    servo-test-code (#3)

---
+++ servo-check/servodemo.ino
@@ -0,0 +1,30 @@
+#include<Servo.h>
+
+Servo myservo;
+
+void setup() 
+{
+myservo.attach(3);  
+
+}
+
+void loop() 
+{
+ 
+ myservo.write(0);
+ delay(250);
+ myservo.write(20);
+ delay(250);
+ myservo.write(40);
+ delay(250);
+  //myservo.detach();
+ myservo.write(60);
+ delay(250);
+
+ myservo.write(80);
+ delay(250);
+ myservo.write(100);
+ delay(250);
+ 
+ 
+}
